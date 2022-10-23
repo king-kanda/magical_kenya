52978e2507e6c2edabcb60a570a3889070128cfb
king-kand <stevenokanda@gmail.com>
2022-10-23 13:45:47 +0300

    starting from aclean slate

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
-import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import 'jquery/dist/jquery.min.js'
-import 'popper.js/dist/popper.min.js'
-import Navbar from './components/navbar/Navbar';
-import 'font-awesome/css/font-awesome.min.css'
-import Carousel from './components/carousel/Carousel';
-import { Newsbar, animateNews } from './components/news-bar/Newsbar';
-import Body from './components/body/Body';
-import Footer from './components/footer/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        {/* component creating navigation bar */}
-        <Navbar />
-
-        {/* component creating image carousel slider */}
-        <Carousel />
-
-        {/* news bar */}
-        <Newsbar />
-
-        <Body />
-
-        <Footer />
-      </div>
-    );
-  }
-
-  componentDidMount() {
-    // DOM manipulation js
-    animateNews();
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
